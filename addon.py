8d294f1eae28da5938b268463cb16439912ad8fb
Peter Reutemann <fracpete@gmail.com>
2020-02-16 10:25:35 +1300

    initial commit, based on https://github.com/zag2me/script.hello.world

---
+++ addon.py
@@ -0,0 +1,17 @@
+import xbmcaddon
+import xbmcgui
+import subprocess
+
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+
+subprocess.run([
+    "kodi-nfo-gen", 
+    "--dir",
+    "/media/xbmc/media/movies",
+    "--recursive",
+    "--fanart",
+    "use-existing",
+])
+
+xbmcgui.Dialog().ok(addonname, ".nfo files generated!")
