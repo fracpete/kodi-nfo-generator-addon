f511ae8a526cc5f6661c2a0e3a8faede5245b91e
Peter Reutemann <github@fracpete.de>
2023-01-05 15:21:01 +1300

    added additional command for tv shows

--- addon.py
+++ addon.py
@@ -5,13 +5,26 @@ import subprocess
 addon     = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 
+# movies
 cmd = [
     "kodi-nfo-gen", 
     "--dir",
     "/media/xbmc/media/movies",
     "--recursive",
     "--fanart",
-    "use-existing",
+    "download-missing",
+]
+code = subprocess.call(cmd)
+
+# tv shows
+cmd = [
+    "kodi-nfo-gen", 
+    "--dir",
+    "/media/xbmc/media/tvshows",
+    "--recursive",
+    "--fanart",
+    "download-missing",
+    "--episodes",
 ]
 code = subprocess.call(cmd)
 
